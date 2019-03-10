04b69f5f6db049bf0350fe1e97c673f142af36ca
superzch <595628677@qq.com>
2019-03-10 23:55:52 +0800

    把本地工程添加到git

---
+++ vue/src/main/java/com/itzch/git.java
@@ -0,0 +1,13 @@
+package com.itzch;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("git,git,git");
+        System.out.println("git,git,git");
+        System.out.println("git,git,git");
+        System.out.println("git,git,git");
+        System.out.println("git,git,git");
+        System.out.println("git,git,git");
+        System.out.println("git,git,git");
+    }
+}
